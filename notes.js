1b27bcda8c8100b731bf7f43307063fe326ce7e9
rajan joshi <rajan.j@lucideustech.com>
2019-05-22 00:01:06 +0530

    duplicate notes with same title filter added

--- notes.js
+++ notes.js
@@ -9,12 +9,21 @@ const getNotes = function(){
 const addNote = function(title,body){
     const notes = loadNotes()
 
-    notes.push({
-        title: title,
-        body: body
+    const duplicateNotes = notes.filter(function(note){
+        return note.title===title;
     })
 
-    saveNotes(notes)
+    if(duplicateNotes.length==0){
+        notes.push({
+            title: title,
+            body: body
+        })
+    
+        saveNotes(notes)
+        console.log('New Note added')
+    }else{
+        console.log('A Note with same title exists!')
+    }
 }
 
 const saveNotes = function(notes){
