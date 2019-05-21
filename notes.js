c852cde31f55e371987425b07f9f109784423a02
rajan4998 <rajan4998@gmail.com>
2019-05-21 23:55:32 +0530

    Notes add and list functionality added

--- notes.js
+++ notes.js
@@ -1,5 +1,40 @@
+const fs = require('fs');
+
+const fileName = 'notes.json';
+
 const getNotes = function(){
     return 'Your notes : ';
 }
 
-module.exports = getNotes;
+const addNote = function(title,body){
+    const notes = loadNotes()
+
+    notes.push({
+        title: title,
+        body: body
+    })
+
+    saveNotes(notes)
+}
+
+const saveNotes = function(notes){
+    const jsonNotes = JSON.stringify(notes);
+    fs.writeFileSync(fileName, jsonNotes)
+}
+
+const loadNotes = function(){
+
+    try {
+        const notes = fs.readFileSync(fileName, 'UTF-8');
+        const dataJson = JSON.parse(notes);
+        return dataJson;
+    }catch(e){
+        return [];
+    }
+}
+
+module.exports = {
+    getNotes : getNotes,
+    addNote : addNote,
+    loadNotes : loadNotes
+};
