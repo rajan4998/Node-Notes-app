eea11ae84c751e781d2238cda6bdeefb30ce5da8
rajan joshi <rajan.j@lucideustech.com>
2019-05-21 23:55:32 +0530

    Notes add and list functionality added

--- app.js
+++ app.js
@@ -1,3 +1,57 @@
-fs = require("fs")
+const yargs = require('yargs');
 
-fs.writeFileSync("notes.txt","This file was creeated by nodejs");
+const notes = require('./notes.js');
+
+yargs.version('1.1.1');
+
+
+yargs.command({
+    command : 'add',
+    describe : 'Add a Note',
+    builder : {
+        title : {
+            describe : 'Title of a Note',
+            demandOption : true,
+            type : 'string'
+        },
+        body : {
+            describe : 'Body of a Note',
+            demandOption : true,
+            type : 'string'
+        }
+    },
+    handler : function(argv){
+        notes.addNote(argv.title, argv.body)
+    }
+});
+
+//create a remove command
+yargs.command({
+    command : 'remove',
+    describe : 'This command removes a Note',
+    handler : function(){
+        console.log('Note removed')
+    }
+})
+
+//create a list command
+yargs.command({
+    command : 'list',
+    describe : 'Shows all the Notes',
+    handler : function(argv){
+        const allNotes = notes.loadNotes()
+        console.log(allNotes)
+    }
+})
+
+//create a read command
+yargs.command({
+    command : 'read',
+    describe : 'This brings a particular Note to read',
+    handler  : function(){
+        console.log('Note read')
+    }
+})
+
+// console.log(yargs.argv)
+yargs.parse()
